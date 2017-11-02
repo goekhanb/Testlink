25e90d1027a0735890cc698f1f97a4d61c8e04ca
goekhan.bastan <goekhan.bastan@udg.de>
2017-11-02 10:18:49 +0100

    unversioned initial commit with "Ghost Inspector" optimized

--- custom/platforms/ghostInspectorView.php
+++ custom/platforms/ghostInspectorView.php
@@ -7,21 +7,20 @@
  */
 
 require_once('../../config.inc.php');
+require_once('FetchData.php');
 
- class GhostInspector {
+ class GhostInspector
+{
 
-    public $url='https://api.ghostinspector.com/v1/suites/59f7159de6fa2c7eb59535d0/tests/?apiKey=adfef11650cdc69490229f96d699764251a5a8c0';
-    public $defaultURL = 'https://ghostinspector.com/';
-    public $d = 'https://goekhan.bastan@udg:Mdtronic"10app.ghostinspector.com/account/login';
 
      public function start()
      {
-         $dataObject =  file_put_contents('myfile.json',file_get_contents($this->url));
+         $fetchData = new FetchData();
+         $dataObject =  file_put_contents('myfile.json',file_get_contents($fetchData->getUrl()));
          $file = json_decode($dataObject);
          var_dump($file);
       }
-
- }
+}
 
 $ghost = new GhostInspector();
 $ghost->start();
